c61884a1d06116847694a3776e645573590087db
Tim Cavey <caveyt@hotmail.co.uk>
2022-09-15 21:15:47 +0100

    Style nav for both pages, add JS click functionality

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 //alert("Testing JS file");
 
 //console.log("testing JS file 2")
+
+//var button = document.getElementById("btn1");
+
+function test () {
+    document.getElementById("navvy").style.visibility = "visible";
+}
+
+document.getElementById("btn1").addEventListener("click", test); 
+
