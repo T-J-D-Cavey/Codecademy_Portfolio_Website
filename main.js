cbf4b65cc4e98e97a145edcb7f78eaecd0b27996
Tim Cavey <caveyt@hotmail.co.uk>
2022-09-18 09:14:57 +0100

    Complete project styling and functionality, ready to merge branches

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-//alert("Testing JS file");
-
-//console.log("testing JS file 2")
-
-//var button = document.getElementById("btn1");
+// Script for both pages nav button
 
 function toggleVisible () {
-    if (document.getElementById("navvy").style.visibility = "hidden") {
-        document.getElementById("navvy").style.visibility = "visible";    
-    } else {
-        document.getElementById("navvy").style.visibility = "hidden";
-    }
+    document.getElementById("navvy").style.visibility = "visible";    
+}
         
-}
-// back up version:
-// function toggleVisible () {
-    // document.getElementById("navvy").style.visibility = "visible";
-    // document.getElementById("navvy").style.visibility = "hidden";
-// }
-
-function toggleHidden () {
-    document.getElementById("navvy").style.visibility = "hidden";
-}
-
 document.getElementById("btn1").addEventListener("click", toggleVisible); 
 
+// Script for project buttons:
+
+
+function toggleVisibleProjectOne () {
+    document.getElementById("card1").style.display = "block";
+}
+        
+document.getElementById("project-btn-1").addEventListener("click", toggleVisibleProjectOne); 
+
+function toggleVisibleProjectTwo () {
+    document.getElementById("card2").style.display = "block";
+}
+        
+document.getElementById("project-btn-2").addEventListener("click", toggleVisibleProjectTwo); 
+
+function toggleVisibleProjectThree () {
+    document.getElementById("card3").style.display = "block";
+}
+        
+document.getElementById("project-btn-3").addEventListener("click", toggleVisibleProjectThree); 
