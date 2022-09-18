02cda650f094421fbfd6b6f3c52411129a102fbd
Tim Cavey <caveyt@hotmail.co.uk>
2022-09-18 14:26:08 +0100

    Fix nav toggle functionality

--- main.js
+++ main.js
@@ -1,41 +1,27 @@
 // Script for both pages nav button
 
-function toggleVisible () {
-    document.getElementById("navvy").style.visibility = "visible";
-    // this.classList.toggle('active');    
+let navList = document.getElementById("navList"); 
+let navBtn = document.getElementById('navBtn');
+
+navBtn.addEventListener("click", toggleNav);
+
+function toggleNav () {
+    if (navList.getAttribute('class')=='displayNone') {
+        navList.setAttribute('class', 'displayBlock')
+    } else {
+        navList.setAttribute('class', 'displayNone')
+    }    
 }
-        
-document.getElementById("btn1").addEventListener("click", toggleVisible); 
+         
 
 // Script for project buttons:
-
-/*
-function toggleVisibleProjectOne () {
-    document.getElementById("card1").style.display = "block";
-}
-        
-document.getElementById("project-btn-1").addEventListener("click", toggleVisibleProjectOne); 
-
-function toggleVisibleProjectTwo () {
-    document.getElementById("card2").style.display = "block";
-}
-        
-document.getElementById("project-btn-2").addEventListener("click", toggleVisibleProjectTwo); 
-
-function toggleVisibleProjectThree () {
-    document.getElementById("card3").style.display = "block";
-}
-        
-document.getElementById("project-btn-3").addEventListener("click", toggleVisibleProjectThree); 
-
-*/
 
 let card1 = document.getElementById("card1");
 let card2 = document.getElementById("card2");
 let card3 = document.getElementById("card3"); 
-let projectBtn1 = document.getElementById("project-btn-1")
-let projectBtn2 = document.getElementById("project-btn-2")
-let projectBtn3 = document.getElementById("project-btn-3")
+let projectBtn1 = document.getElementById("project-btn-1");
+let projectBtn2 = document.getElementById("project-btn-2");
+let projectBtn3 = document.getElementById("project-btn-3");
 
 projectBtn1.addEventListener('click', toggleDisplayBlock1);
 
@@ -56,15 +42,13 @@ function toggleDisplayBlock2 () {
         card2.setAttribute('class', 'displayNone')
     } else {
         card2.setAttribute('class', 'displayBlock')
-    }
-        
-    }
+    }       
+    };
 
 function toggleDisplayBlock3 () {
 if (card3.getAttribute('class')=='displayBlock') {
     card3.setAttribute('class', 'displayNone')
 } else {
     card3.setAttribute('class', 'displayBlock')
-}
-    
-}
+}  
+};
